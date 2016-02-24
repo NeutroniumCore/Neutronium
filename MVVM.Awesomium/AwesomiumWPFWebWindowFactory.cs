dfcab369210e37325fe8482d57b45a9abf732dad
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-02-24 12:58:28 -0300

    Update DotNet version for knockout test framework

--- MVVM.Awesomium/AwesomiumWPFWebWindowFactory.cs
+++ MVVM.Awesomium/AwesomiumWPFWebWindowFactory.cs
@@ -1,11 +1,6 @@
 ﻿using Awesomium.Core;
 using HTML_WPF.Component;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MVVM.Awesomium
 {
