0fe664d2cd51ff2adca150c839a9582fa64cbe63
David Desmaisons <desmaisons_david@hotmail.com>
2016-02-28 23:37:12 -0300

    Improving tests

--- Tests/MVVM.Awesomium.Tests/Integrated/Test_HTMLBinding_Awe.cs
+++ Tests/MVVM.Awesomium.Tests/Integrated/Test_HTMLBinding_Awe.cs
@@ -16,14 +16,21 @@ using IntegratedTest;
 using IntegratedTest.TestData;
 using IntegratedTest.Windowless;
 using MVVM.Cef.Glue.Test.Infra;
+using Xunit;
 
 namespace MVVM.Awesomium.Tests.Integrated
 {
+    //[Fact]
     public class Test_HTMLBinding_Awe : Test_HTMLBinding
     {
+        
         public Test_HTMLBinding_Awe() : base(AwesomiumTestHelper.GetWindowlessEnvironment())
         {
         }
+
+        [Fact]
+        public void Test()
+        { }
     }
 }
 
