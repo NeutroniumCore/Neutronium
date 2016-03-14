ecf16eab7733ed21e30fff307d73792053850df7
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-03-14 13:53:15 -0300

    Creating first version of wpf control

---
+++ HTMEngine.ChromiumFX/WPF/ChromiumFxControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using HTML_WPF.Component;
+using MVVM.HTML.Core;
+using MVVM.HTML.Core.Exceptions;
+using MVVM.HTML.Core.Infra.VM;
+using MVVM.HTML.Core.JavascriptEngine.Control;
+using MVVM.HTML.Core.JavascriptEngine.Window;
+using MVVM.HTML.Core.JavascriptUIFramework;
+using MVVM.HTML.Core.Navigation;
+
+namespace HTMEngine.ChromiumFX.WPF
+{
+    public partial class ChromiumFxControl 
+    {
+        protected ChromiumFxControl(IUrlSolver iIUrlSolver) 
+        {
+            InitializeComponent();
+        }
+    }
+}
