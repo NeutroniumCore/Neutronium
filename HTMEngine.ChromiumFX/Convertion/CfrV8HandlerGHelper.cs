faa041abc5ea88e4a71cc05711dc2aefc96ff215
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-03-15 13:37:10 -0300

    Implemented ChromiumFXJavascriptObject

---
+++ HTMEngine.ChromiumFX/Convertion/CfrV8HandlerGHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Chromium.Remote;
+using MVVM.HTML.Core.JavascriptEngine.JavascriptObject;
+
+namespace HTMEngine.ChromiumFX.Convertion {
+    public static class CfrV8HandlerGHelper 
+    {
+        public static CfrV8Handler Convert(this Action<string, IJavascriptObject, IJavascriptObject[]> function, string name) 
+        {
+            var res = new CfrV8Handler();
+            res.Execute += (o, e) => function(name, e.Object.Convert(), e.Arguments.Convert());
+            return res;
+        }
+    }
+}
