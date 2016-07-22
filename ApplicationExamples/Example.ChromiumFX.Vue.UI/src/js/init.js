91f8440da1b12f456664ec4278c13afb3b46e04a
desmaisons_david <desmaisons_david@hotmail.com>
2016-07-21 22:56:03 -0300

    Prototype image binding

--- ApplicationExamples/Example.ChromiumFX.Vue.UI/src/js/init.js
+++ ApplicationExamples/Example.ChromiumFX.Vue.UI/src/js/init.js
@@ -1,7 +1,31 @@
-﻿(function () {
-    console.log('init');
-    ko.Enumimages = { Sex: { Male: 'images/male.png', Female: 'images/sem%20t%c3%adtulo.png' } };
+﻿//(function () {
+//    console.log('init');
+//    ko.Enumimages = { Sex: { Male: 'images/male.png', Female: 'images/sem%20t%c3%adtulo.png' } };
+//}());
+
+(function () {
+    var images = {
+         Sex: { Male: 'images/male.png', Female: 'images/sem%20t%c3%adtulo.png' } 
+    };
+
+    Vue._vmMixin = {
+        methods:{
+            enumImage: function(value){
+                if (!value instanceof Enum)
+                    return null;
+
+                var ec = images[value.type];
+                return ec ? ec[value.name] : null;
+            }
+        },
+        computed: {
+            count: function () {
+                return this.Skills.length;
+            }
+        }
+    }
 }());
+
 
 
 
