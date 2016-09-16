817f506a478dec3c8920edf7a2ee44c3c9b6cf00
desmaisons_david <desmaisons_david@hotmail.com>
2016-09-16 00:05:51 -0300

    prototyping promise vue window

---
+++ Examples/Examples.ChromiumFx.Vue.PromiseDemo/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Examples.ChromiumFx.Vue.PromiseDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Examples/Examples.ChromiumFx.Vue.PromiseDemo/View/js/Init.js
@@ -0,0 +1,30 @@
+﻿(function () {
+
+    ko.register = function (vm) {
+        vm.factotyresult = ko.observable(null);
+        vm.lastError = ko.observable(null);
+
+        vm.result = function (res) {
+            alert(res.LastName());
+            vm.factotyresult(res);
+        };
+
+        vm.error = function (err) {
+            vm.lastError(err);
+            console.log(err);
+            alert(err);
+        };
+
+        vm.click = function () {
+            executeAsPromise(vm, 'CreateObject',vm.Name()).then(
+                function (res) {
+                    alert(res.LastName());
+                    vm.factotyresult(res);
+                }
+            ).catch(function(reason) {
+                console.log(reason);
+                alert(reason);
+            });
+        };
+    };
+})()
