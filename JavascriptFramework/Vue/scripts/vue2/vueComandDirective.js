d1c1574b2269c122db8f089e038ecd8943963a79
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-10-03 13:36:31 -0300

    Adding arg to Vue.js 2.0 command directive

--- JavascriptFramework/Vue/scripts/vue2/vueComandDirective.js
+++ JavascriptFramework/Vue/scripts/vue2/vueComandDirective.js
@@ -3,53 +3,60 @@
 
     function createDirective(execute) {
 
-        function createListener(el, evt, value, arg) {
-            if (!value)
+        function createListener(el, evt, binding) {
+            if (!binding.value)
                 return null;
 
+            var command, arg = null;
+            if (!!binding.value.arg) {
+                command = binding.value.command;
+                arg = binding.value.arg;
+            } else {
+                command = binding.value;
+            }
+
             var res = function callBack() {
-                execute(value, arg);
+                execute(command, arg);
             };
             el.addEventListener(evt, res);
             return res;
         }
 
         function deleteListener(el, evt, callback) {
-            if (!!callBack) {
-                el.removeEventListener(evt, callBack);
+            if (!!callback) {
+                el.removeEventListener(evt, callback);
             }
+        }
+
+        function getEvent(binding) {
+            return binding.arg || "click";
         }
 
         return {
             bind: function (el, binding) {
-                const evt = binding.arg || "click";
-                var arg = null;
-                el.__callBack = createListener(el, evt, binding.value, arg);
+                const evt = getEvent(binding);
+                el.__callBack = createListener(el, evt, binding);
             },
             update: function (el, binding) {
-                if (binding.value == binding.oldValue)
-                    return;
-                const evt = binding.arg || "click";
-                deleteListener(el, evt, el.__callBack);
-                //var arg = ctx.params.commandarg || null;
-                var arg = null;
-                el.__callBack = createListener(el, evt, binding.value, arg);
+                const evt = getEvent(binding);
+                deleteListener(el, evt, el.__callBack);             
+                el.__callBack = createListener(el, evt, binding);
             },
             unbind: function (el, binding) {
-                const evt = binding.arg || "click";
+                const evt = getEvent(binding);
                 deleteListener(el, evt, el.__callBack);
             }
         };
     }
 
-    var comand = createDirective(function (newValue, arg) {
-        newValue.CanExecute(arg);
-        if (newValue.CanExecuteValue)
-            newValue.Execute(arg);
+    var comand = createDirective(function (command, arg) {
+        command.CanExecute(arg);
+        if (command.CanExecuteValue)
+            command.Execute(arg);
     });
 
-    var simpleComand = createDirective(function (newValue, arg) {
-        newValue.Execute(arg);
+    var simpleComand = createDirective(function (command, arg) {
+        command.Execute(arg);
     });
 
     Vue.directive("command", comand);
