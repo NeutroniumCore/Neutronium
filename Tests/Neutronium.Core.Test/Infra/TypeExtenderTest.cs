4a211a950188407ba150ecf920bd80f1e97753bf
desmaisons_david <desmaisons_david@hotmail.com>
2016-10-08 23:57:41 -0300

    Improving tests

--- Tests/Neutronium.Core.Test/Infra/TypeExtenderTest.cs
+++ Tests/Neutronium.Core.Test/Infra/TypeExtenderTest.cs
@@ -10,54 +10,68 @@ namespace Neutronium.Core.Test.Infra
     {
 
         [Fact]
-        public void Test_GetEnumerableBase_int_isNotEnumerable()
+        public void GetEnumerableBase_int_isNotEnumerable()
         {
             typeof(int).GetEnumerableBase().Should().BeNull();
         }
 
         [Fact]
-        public void Test_GetEnumerableBase_IEnumerable_int_isNotEnumerable()
+        public void GetEnumerableBase_IEnumerable_int_isNotEnumerable()
         {
             typeof(IEnumerable<int>).GetEnumerableBase().Should().Be(typeof(int));
         }
 
         [Fact]
-        public void Test_GetEnumerableBase_IList_int_isNotEnumerable()
+        public void GetEnumerableBase_IList_int_isNotEnumerable()
         {
             typeof(IList<int>).GetEnumerableBase().Should().Be(typeof(int));
         }
 
         [Fact]
-        public void Test_GetEnumerableBase_null_int_isNotEnumerable()
+        public void GetEnumerableBase_null_int_isNotEnumerable()
         {
             Type n = null;
             n.GetEnumerableBase().Should().BeNull();
         }
 
         [Fact]
-        public void Test_GetUnderlyingNullableType_null()
+        public void GetUnderlyingNullableType_null()
         {
             Type n = null;
             n.GetUnderlyingNullableType().Should().BeNull();
         }
 
         [Fact]
-        public void Test_GetUnderlyingiliststring_null()
+        public void GetUnderlyingiliststring_null()
         {
             Type n = typeof(IList<string>);
             n.GetUnderlyingNullableType().Should().BeNull();
         }
 
         [Fact]
-        public void Test_GetUnderlyingNullableType_int()
+        public void GetUnderlyingNullableType_int()
         {
             typeof(Nullable<int>).GetUnderlyingNullableType().Should().Be(typeof(int));
         }
 
         [Fact]
-        public void Test_GetUnderlyingNullableType_string()
+        public void GetUnderlyingNullableType_string()
         {
             typeof(string).GetUnderlyingNullableType().Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData(typeof(UInt16), true)]
+        [InlineData(typeof(UInt32), true)]
+        [InlineData(typeof(UInt64), true)]
+        [InlineData(typeof(Int16), false)]
+        [InlineData(typeof(Int32), false)]
+        [InlineData(typeof(Int64), false)]
+        [InlineData(null, false)]
+        public void IsUnsigned_returns_expected_value(Type type, bool result)
+        {
+            var isUnsigned = type.IsUnsigned();
+            isUnsigned.Should().Be(result);
         }
     }
 }
