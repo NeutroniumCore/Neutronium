3f235beb94cdc5883330bdd26e3658f32a74639d
desmaisons_david <desmaisons_david@hotmail.com>
2017-02-18 17:13:23 -0200

    Symplify cfx render process

--- WebBrowserEngine/ChromiumFX/ChromiumFXRenderProcess/Program.cs
+++ WebBrowserEngine/ChromiumFX/ChromiumFXRenderProcess/Program.cs
@@ -14,15 +14,8 @@ namespace ChromiumFXRenderProcess
         static void Main() 
         {
             CfxRuntime.LibCefDirPath = @"cef\Release";
-            ChromiumWebBrowser.OnBeforeCfxInitialize += ChromiumWebBrowser_OnBeforeCfxInitialize;
             int retval = CfxRuntime.ExecuteProcess();
             Environment.Exit(retval);
-        }
-
-        private static void ChromiumWebBrowser_OnBeforeCfxInitialize(OnBeforeCfxInitializeEventArgs e)
-        {
-            e.Settings.LocalesDirPath = System.IO.Path.GetFullPath(@"cef\Resources\locales");
-            e.Settings.ResourcesDirPath = System.IO.Path.GetFullPath(@"cef\Resources");
         }
     }
 }
