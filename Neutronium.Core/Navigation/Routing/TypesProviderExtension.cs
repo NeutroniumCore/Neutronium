9cafb347d35295c4632e0e127edc910d5d5a12ef
desmaisons_david <desmaisons_david@hotmail.com>
2017-03-22 14:34:59 -0300

    Improving routing Apis

--- Neutronium.Core/Navigation/Routing/TypesProviderExtension.cs
+++ Neutronium.Core/Navigation/Routing/TypesProviderExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Neutronium.Core.Navigation.Routing
@@ -10,14 +11,20 @@ namespace Neutronium.Core.Navigation.Routing
             return new TypesProvider(assembly.GetTypes());
         }
 
-        public static ITypesProvider GetAllTypesFromAssembly(this object @object) 
+        public static ITypesProvider GetAllTypesFromAssembly(this Type type)
         {
-            return GetAllTypes(@object.GetType().Assembly);
+            return GetAllTypes(type.Assembly);
+        }
+
+        public static ITypesProvider GetAllTypesFromAssembly(this object @object)
+        {
+            return GetAllTypesFromAssembly(@object.GetType());
         }
 
         public static ITypesProvider Implementing<T>(this ITypesProvider typeProvider)
         {
-            return new TypesProvider(typeProvider.Types.Where(t => t.IsAssignableFrom(typeof(T))));
+            var type = typeof(T);
+            return new TypesProvider(typeProvider.Types.Where(t => type != t && type.IsAssignableFrom(t)));
         }
 
         public static ITypesProvider WithNameContaining(this ITypesProvider typeProvider, string value)
@@ -25,14 +32,44 @@ namespace Neutronium.Core.Navigation.Routing
             return new TypesProvider(typeProvider.Types.Where(t => t.Name.Contains(value)));
         }
 
+        public static ITypesProvider WithNameEndingWith(this ITypesProvider typeProvider, string value)
+        {
+            return new TypesProvider(typeProvider.Types.Where(t => t.Name.EndsWith(value)));
+        }
+
         public static ITypesProvider InNamespace(this ITypesProvider typeProvider, string value)
         {
             return new TypesProvider(typeProvider.Types.Where(t => t.Namespace == value));
         }
 
-        public static ITypesProvider And(this ITypesProvider typeProvider, ITypesProvider typeProvider2)
+        public static ITypesProvider Add(this ITypesProvider typeProvider, ITypesProvider typeProvider2)
         {
             return new TypesProvider(typeProvider.Types.Concat(typeProvider2.Types));
+        }
+
+        public static ITypesProvider Add(this ITypesProvider typeProvider, Assembly assembly)
+        {
+            return typeProvider.Add(assembly.GetAllTypes());
+        }
+
+        public static ITypesProvider Add(this ITypesProvider typeProvider, Type type)
+        {
+            return typeProvider.Add(type.GetAllTypesFromAssembly());
+        }
+
+        public static ITypesProvider Add(this ITypesProvider typeProvider, object @object)
+        {
+            return typeProvider.Add(@object.GetAllTypesFromAssembly());
+        }
+
+        public static ITypesProvider Add(this ITypesProvider typeProvider, params Type[] types)
+        {
+            return new TypesProvider(typeProvider.Types.Concat(types));
+        }
+
+        public static ITypesProvider Where(this ITypesProvider typeProvider, Func<Type, bool> filter)
+        {
+            return new TypesProvider(typeProvider.Types.Where(filter));
         }
 
         public static IConventionRouter Register(this IConventionRouter conventionRouter, ITypesProvider typeProvider)
--- Tests/Neutronium.Core.Test/Navigation/Routing/TypeProviderTest.cs
+++ Tests/Neutronium.Core.Test/Navigation/Routing/TypeProviderTest.cs
@@ -1,5 +1,8 @@
 ﻿using FluentAssertions;
 using Neutronium.Core.Navigation.Routing;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Xunit;
 
 namespace Neutronium.Core.Test.Navigation.Routing
@@ -8,15 +11,47 @@ namespace Neutronium.Core.Test.Navigation.Routing
     {
         public interface IInterface { }
 
+        public interface IInterface2 { }
+
         public class Class1: IInterface { }
 
-        public class Class2 : IInterface { }
+        public class Class2 : IInterface, IInterface2 { }
+
+        private IEnumerable<Type> TestAssemblyTypes => typeof(TypeProviderTest).Assembly.GetTypes();
 
         [Fact]
-        public void Implementing_filter_type_by_inheritance() 
+        public void GetAllTypesFromAssembly_object_returns_types_of_assembly()
+        {
+            var typeProvider = this.GetAllTypesFromAssembly();
+            typeProvider.Types.Should().BeEquivalentTo(TestAssemblyTypes);
+        }
+
+        [Fact]
+        public void GetAllTypesFromAssembly_type_returns_types_of_assembly()
+        {
+            var typeProvider = GetType().GetAllTypesFromAssembly();
+            typeProvider.Types.Should().BeEquivalentTo(TestAssemblyTypes);
+        }
+
+        [Fact]
+        public void GetAllTypes_returns_types_of_assembly()
+        {
+            var typeProvider = Assembly.GetExecutingAssembly().GetAllTypes();
+            typeProvider.Types.Should().BeEquivalentTo(TestAssemblyTypes);
+        }
+
+        [Fact]
+        public void Implementing_filters_type_by_inheritance() 
         {
             var typeProvider = this.GetAllTypesFromAssembly().Implementing<IInterface>();
             typeProvider.Types.Should().BeEquivalentTo(typeof(Class1), typeof(Class2));
-        } 
+        }
+
+        [Fact]
+        public void Implementing_filters_type_by_inheritance_case_2()
+        {
+            var typeProvider = this.GetAllTypesFromAssembly().Implementing<IInterface2>();
+            typeProvider.Types.Should().BeEquivalentTo(typeof(Class2));
+        }
     }
 }
