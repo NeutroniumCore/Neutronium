ad8b0c2be17b6a731690ffb1b601ddcd2619a1c7
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2017-04-06 20:05:09 -0300

    chromeless behaviour

--- Neutronium.WPF/Windows/Chromeless.cs
+++ Neutronium.WPF/Windows/Chromeless.cs
@@ -1,9 +1,48 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interactivity;
+using System.Windows.Shell;
 
 namespace Neutronium.WPF.Windows 
 {
-    public class Chromeless : Behavior<Window> 
+    public class Chromeless : Behavior<Window>
     {
+        private Window Window => AssociatedObject;
+
+        protected override void OnAttached() 
+        {
+            base.OnAttached();
+
+            Window.LocationChanged += ChromelessWindow_LocationChanged;
+            ApplyStyle();
+        }
+
+        private void ApplyStyle() 
+        {
+            var style = new Style(typeof(Window));
+            style.Setters.Add(new Setter(Control.BorderThicknessProperty, 1));
+            var windowChrome = new WindowChrome {
+                ResizeBorderThickness = new Thickness(6),
+                CaptionHeight = 0,
+                CornerRadius = new CornerRadius(2,2,1,1),
+                GlassFrameThickness = new Thickness(0)
+            };
+            style.Setters.Add(new Setter(WindowChrome.WindowChromeProperty, windowChrome));
+            Window.Style = style;
+        }
+
+        private void ChromelessWindow_LocationChanged(object sender, EventArgs e) 
+        {
+            var area = Window.GetCurrentScreenWorkingArea();
+            Window.MaxHeight = area.Height;
+            Window.MaxWidth = area.Width;
+        }
+
+        protected override void OnDetaching() 
+        {
+            base.OnDetaching();
+            Window.LocationChanged -= ChromelessWindow_LocationChanged;
+        }
     }
 }
