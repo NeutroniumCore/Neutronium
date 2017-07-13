2962d052e888560c8e6b9140e52e8b5f61ddc302
desmaisons_david <desmaisons_david@hotmail.com>
2017-07-12 21:46:53 -0300

    taking out cef vue performance tests + adding Cfx

--- Tests/JavascriptFramework/Vuejs/Vue.Binding.CefGlue.Tests/BindingPerformance_Vue_Cef_Tests.cs
+++ Tests/JavascriptFramework/Vuejs/Vue.Binding.CefGlue.Tests/BindingPerformance_Vue_Cef_Tests.cs
@@ -1,30 +1,30 @@
-﻿using Vue.Binding.CefGlue.Tests.Infra;
-using System.Collections.Generic;
-using Tests.Universal.HTMLBindingTests;
-using Xunit;
-using Xunit.Abstractions;
+﻿//using Vue.Binding.CefGlue.Tests.Infra;
+//using System.Collections.Generic;
+//using Tests.Universal.HTMLBindingTests;
+//using Xunit;
+//using Xunit.Abstractions;
 
-namespace Vue.Binding.CefGlue.Tests
-{
-    [Collection("Cef Vue Windowless Integrated")]
-    public class BindingPerformance_Vue_Cef_Tests : HTMLBindingPerformanceTests
-    {
-        public BindingPerformance_Vue_Cef_Tests(CefGlueVueContext context, ITestOutputHelper output)
-            : base(context, output, GetKoCefPerformanceExpectations())
-        {
-        }
+//namespace Vue.Binding.CefGlue.Tests
+//{
+//    [Collection("Cef Vue Windowless Integrated")]
+//    public class BindingPerformance_Vue_Cef_Tests : HTMLBindingPerformanceTests
+//    {
+//        public BindingPerformance_Vue_Cef_Tests(CefGlueVueContext context, ITestOutputHelper output)
+//            : base(context, output, GetKoCefPerformanceExpectations())
+//        {
+//        }
 
-        private static Dictionary<TestPerformanceKind, int> GetKoCefPerformanceExpectations()
-        {
-            return new Dictionary<TestPerformanceKind, int>
-            {
-                {TestPerformanceKind.OneTime_Collection_CreateBinding, 1500},
-                {TestPerformanceKind.TwoWay_Collection_CreateBinding, 1500},
-                {TestPerformanceKind.OneWay_Collection_CreateBinding, 1500},
-                {TestPerformanceKind.TwoWay_Int, 3100},
-                {TestPerformanceKind.TwoWay_Collection, 4700},
-                {TestPerformanceKind.TwoWay_Collection_Update_From_Javascript, 150}
-            };
-        }
-    }
-}
+//        private static Dictionary<TestPerformanceKind, int> GetKoCefPerformanceExpectations()
+//        {
+//            return new Dictionary<TestPerformanceKind, int>
+//            {
+//                {TestPerformanceKind.OneTime_Collection_CreateBinding, 1500},
+//                {TestPerformanceKind.TwoWay_Collection_CreateBinding, 1500},
+//                {TestPerformanceKind.OneWay_Collection_CreateBinding, 1500},
+//                {TestPerformanceKind.TwoWay_Int, 3100},
+//                {TestPerformanceKind.TwoWay_Collection, 4700},
+//                {TestPerformanceKind.TwoWay_Collection_Update_From_Javascript, 150}
+//            };
+//        }
+//    }
+//}
