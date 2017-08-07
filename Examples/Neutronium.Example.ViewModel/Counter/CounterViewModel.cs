d04e7606cc844e9329f8bfc4ecfb4c081b6ebe15
desmaisons_david <desmaisons_david@hotmail.com>
2017-08-07 00:26:34 -0300

    Adding example projects to better mesure performance

---
+++ Examples/Neutronium.Example.ViewModel/Counter/CounterViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Neutronium.Example.ViewModel.Infra;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System;
+
+namespace Neutronium.Example.ViewModel.Counter
+{
+    public class CounterViewModel : ViewModelBase
+    {
+        public int CounterRead { get; private set; }
+
+        private int _Counter;
+        public int Counter
+        {
+            get
+            {
+                CounterRead++;
+                return _Counter;
+            }
+            set
+            {
+                Set(ref _Counter, value, "Counter");
+            }
+        }
+
+        public ICommand Count { get; }
+
+        public IProgress<int> Progess { get; set; }
+
+        public CounterViewModel()
+        {
+            Count = new RelayCommand(DoCount);
+        }
+
+        private void DoCount()
+        {
+            Task.Run(() =>
+            {
+                for(var i=0; i<10000; i++)
+                {
+                    Progess?.Report(i);
+                }
+            });
+        }
+    }
+}
---
+++ WpfReferenceApplication/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Neutronium.Example.ViewModel.Counter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfReferenceApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CounterViewModel _Counter;
+        public MainWindow()
+        {
+            _Counter = new CounterViewModel();
+            DataContext = _Counter;
+            InitializeComponent();
+            _Counter.Progess = new Progress<int>(i => _Counter.Counter = i);
+        }
+    }
+}
