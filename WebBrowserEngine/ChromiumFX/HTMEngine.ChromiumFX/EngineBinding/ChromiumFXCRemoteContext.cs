7d26396ae743029ddf8021b6f5d104b46f9c2c61
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2017-08-09 17:17:44 -0300

    Using queue to optimize context switch

--- WebBrowserEngine/ChromiumFX/HTMEngine.ChromiumFX/EngineBinding/ChromiumFXCRemoteContext.cs
+++ WebBrowserEngine/ChromiumFX/HTMEngine.ChromiumFX/EngineBinding/ChromiumFXCRemoteContext.cs
@@ -7,18 +7,16 @@ namespace Neutronium.WebBrowserEngine.ChromiumFx.EngineBinding
     internal struct ChromiumFxCRemoteContext : IDisposable 
     {
         private readonly CfxRemoteCallContext _CfxRemoteCallContext;
-        internal bool IsInContext { get; }
 
         public ChromiumFxCRemoteContext(CfrBrowser browser) 
         {
-            IsInContext = CfxRemoteCallContext.IsInContext;
-            _CfxRemoteCallContext = IsInContext ? null : browser.CreateRemoteCallContext();
-            _CfxRemoteCallContext?.Enter();
+            _CfxRemoteCallContext = browser.CreateRemoteCallContext();
+            _CfxRemoteCallContext.Enter();
         }
 
         public void Dispose() 
         {
-            _CfxRemoteCallContext?.Exit();
+            _CfxRemoteCallContext.Exit();
         }
     }
 }
