44594627e17dd5cf1aeb16663bf5b3706402722a
lhyqy5 <lhyqy5@gmail.com>
2017-08-19 13:27:11 +0800

    add pack uri example project

---
+++ Examples/Example.ChromiumFX.PackUri/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Neutronium.JavascriptFramework.Vue;
+using Neutronium.WPF;
+
+namespace Example.ChromiumFX.PackUri
+{
+    /// <summary>
+    /// Interaction logic for CefGlueApp.xaml
+    /// </summary>
+    public partial class WebBrowserApp
+    {
+        protected override void OnStartUp(IHTMLEngineFactory factory)
+        {
+            factory.RegisterJavaScriptFramework(new VueSessionInjectorV2());
+            base.OnStartUp(factory);
+        }
+    }
+}
