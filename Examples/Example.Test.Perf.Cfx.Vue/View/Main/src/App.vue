3eb24f6d3a5f1f78ff8e653e647e796c3bf94166
desmaisons_david <desmaisons_david@hotmail.com>
2017-08-20 12:31:50 -0300

    Improving vue component for performance test example

--- Examples/Example.Test.Perf.Cfx.Vue/View/Main/src/App.vue
+++ Examples/Example.Test.Perf.Cfx.Vue/View/Main/src/App.vue
@@ -1,69 +1,68 @@
 <template>
-<div id="app" class="fluid container">
-  <div id="main-menu" class="jumbotron logo">
-    <img src="./assets/logo.png">
-    <p>Neutronium Demo Perf Application</p>
-  </div>
+    <div id="app" class="fluid container">
+        <div id="main-menu" class="jumbotron logo">
+            <img src="./assets/logo.png">
+            <p>Neutronium Demo Perf Application</p>
+        </div>
 
- <div class="list-group col-md-3">
-    <div>
-      {{Counter}}
-    </div>
+        <div class="list-group col-md-3">
+            <counter :counter="Counter" :state="State" :command="Count">
+            </counter>
+        </div>
 
-    <div>
-      {{State}}
-    </div>
-    <command-button :command="Count" name="Run"></command-button>
-  </div>
+        <div class="list-group col-md-3">
+            <raw-display :object="bigVm">
+            </raw-display>
+        </div>
 
-  <div class="list-group col-md-3">
-    <pre>{{bigVm}}</pre>
-  </div>
+        <command-button :command="BuildBigModel" name="Build Big Model"></command-button>
 
-  
-  <command-button :command="BuildBigModel" name="Build Big Model"></command-button>
-
-</div>
+        </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/less/font-awesome.less';
 import commandButton from './components/command-button'
+import counter from './components/counter'
+import rawDisplay from './components/RawDisplay'
 
-const props={
-  viewModel: Object,
-};
+
+    const props={
+        viewModel: Object,
+    };
 
 export default {
-  name: 'app',
-  props,
-  components:{
-    commandButton
-  },
+    name: 'app',
+    props,
+        components:{
+            commandButton,
+            counter,
+            rawDisplay
+        },
   data () {
-    return this.viewModel
+      return this.viewModel
   },
-  computed: {
-    bigVm() {
-        return JSON.stringify(this.viewModel.Big, null, 2);  
+    computed: {
+        bigVm() {
+            return JSON.stringify(this.viewModel.Big, null, 2);
+        }
     }
-  }
-}
+    }
 </script>
 
 <style>
-  #app .logo{
-  text-align: center;
-  }
+    #app .logo {
+        text-align: center;
+    }
 
-  #app .logo img{
-  width: 100px;
-  }
+        #app .logo img {
+            width: 100px;
+        }
 
-  .panel-body {
-  position: relative;
-  min-height:200px;
-  height:100%;
-  }
+    .panel-body {
+        position: relative;
+        min-height: 200px;
+        height: 100%;
+    }
 </style>
---
+++ Examples/Example.Test.Perf.Cfx.Vue/View/Main/src/components/counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>
+            {{counter}}
+        </div>
+
+        <div>
+            {{state}}
+        </div>
+        <command-button :command="command" name="Run"></command-button>
+    </div>
+</template>
+
+<script>
+import commandButton from './command-button'
+
+export default {
+  props:{
+      counter: Number,
+      state: String,
+      command: Object
+  },
+   components:{
+    commandButton
+  }
+}
+</script>
+
+<style>
+</style>
