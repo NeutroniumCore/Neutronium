9bdcdc41a869f2302050e330553555d5b4dcaaed
desmaisons_david <desmaisons_david@hotmail.com>
2017-10-18 00:36:21 -0200

    Updgrading to vue 2.5.2 + vue-devtool 3.1.3+ (02826962f8c4fa76841e900416e54596080f9dba)

--- JavascriptFramework/Vue/VueVersions.cs
+++ JavascriptFramework/Vue/VueVersions.cs
@@ -12,7 +12,7 @@ namespace Neutronium.JavascriptFramework.Vue
 
         internal static VueVersion Vue1 { get; } = new VueVersion("1.0.25", "VueInjector", "vue1", null);
 
-        internal static VueVersion Vue2 { get; } = new VueVersion("2.2.5", "VueInjectorV2", "vue2", Vue2DebugToolsUI);
+        internal static VueVersion Vue2 { get; } = new VueVersion("2.5.2", "VueInjectorV2", "vue2", Vue2DebugToolsUI);
 
         private static DebugToolsUI Vue2DebugToolsUI => new DebugToolsUI(Vue2DebugTools, Vue2About);
 
