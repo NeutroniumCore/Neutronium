95e969b1c5f4e0d70adad8d48d1c9dd103a1c00b
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2017-10-25 14:54:04 -0200

    Improvimg vue-i18n example

--- Examples/Example.Option.CFx.Vue/View/Main/src/install.js
+++ Examples/Example.Option.CFx.Vue/View/Main/src/install.js
@@ -1,22 +1,5 @@
 import VueI18n from 'vue-i18n'
-
-const messages = {
-  en: {
-    message: {
-      hello: 'hello world'
-    }
-  },
-  ja: {
-    message: {
-      hello: 'こんにちは、世界'
-    }
-  },
-  ru: {
-    message: {
-        hello: "привет, мир!"
-    }
-  }
-}
+import {messages} from './messages'
 
 function install(vue) {
     //Call vue use here if needed
---
+++ Examples/Example.Option.CFx.Vue/View/Main/src/messages.js
@@ -0,0 +1,21 @@
+const messages = {
+    en: {
+      message: {
+        hello: 'hello world'
+      }
+    },
+    ja: {
+      message: {
+        hello: 'こんにちは、世界'
+      }
+    },
+    ru: {
+      message: {
+          hello: "привет, мир!"
+      }
+    }
+  }
+
+  export {
+    messages
+  }
