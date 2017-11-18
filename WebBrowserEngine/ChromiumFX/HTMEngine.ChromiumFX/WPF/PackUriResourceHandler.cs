b0edbd77f69026a2fe1f3915025f1efdcd01384e
desmaisons_david <desmaisons_david@hotmail.com>
2017-11-17 22:20:32 -0200

    Improve pack uri handler

--- WebBrowserEngine/ChromiumFX/HTMEngine.ChromiumFX/WPF/PackUriResourceHandler.cs
+++ WebBrowserEngine/ChromiumFX/HTMEngine.ChromiumFX/WPF/PackUriResourceHandler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Windows.Resources;
 using Chromium;
 using Neutronium.Core;
@@ -14,26 +16,26 @@ namespace Neutronium.WebBrowserEngine.ChromiumFx.WPF
             {
                 resInfo = System.Windows.Application.GetResourceStream(packUri);
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 logger?.Error(() => $"Unable to find pack ressource:{packUri} exception:{exception}");
             }
 
             GetResponseHeaders += (s1, e1) =>
             {
-                if (resInfo != null)
-                {
-                    e1.ResponseLength = resInfo.Stream.Length;
-                    e1.Response.MimeType = resInfo.ContentType;
-                    e1.Response.Status = 200;
-                    e1.Response.StatusText = "OK";
-                }
-                else
+                if (resInfo == null)
                 {
                     e1.Response.Status = 404;
                     e1.Response.StatusText = "Not Found";
+                    return;
                 }
+
+                e1.ResponseLength = resInfo.Stream.Length;
+                e1.Response.MimeType = GetMineType(resInfo, packUri);
+                e1.Response.Status = 200;
+                e1.Response.StatusText = "OK";
             };
+
             ReadResponse += (s2, e2) =>
             {
                 if (resInfo != null)
@@ -42,15 +44,25 @@ namespace Neutronium.WebBrowserEngine.ChromiumFx.WPF
                     var bytesRead = resInfo.Stream.Read(buffer, 0, e2.BytesToRead);
                     System.Runtime.InteropServices.Marshal.Copy(buffer, 0, e2.DataOut, bytesRead);
                     e2.BytesRead = bytesRead;
-                    e2.Callback.Continue();
                 }
                 e2.SetReturnValue(true);
             };
+
             ProcessRequest += (s3, e3) =>
             {
                 e3.Callback.Continue();
                 e3.SetReturnValue(true);
             };
+        }
+
+        private static string GetMineType(StreamResourceInfo streamResourceInfo, Uri uri)
+        {
+            var type = streamResourceInfo.ContentType;
+            if (!string.IsNullOrEmpty(type))
+                return type;
+
+            var extension = Path.GetExtension(uri.AbsoluteUri).Replace(".", "");
+            return CfxRuntime.GetMimeType(extension);
         }
     }
 }
