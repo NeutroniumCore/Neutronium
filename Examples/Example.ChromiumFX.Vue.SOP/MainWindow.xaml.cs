8e7f79fc06609cbb3b9a285fd536ec5a6d4a8b77
desmaisons_david <desmaisons_david@hotmail.com>
2017-12-31 12:59:09 -0200

    Adding Print example to Example.ChromiumFX.Vue.SOP

--- Examples/Example.ChromiumFX.Vue.SOP/MainWindow.xaml.cs
+++ Examples/Example.ChromiumFX.Vue.SOP/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using Neutronium.WebBrowserEngine.ChromiumFx;
 using System.Diagnostics;
+using Neutronium.WebBrowserEngine.ChromiumFx.EngineBinding;
 
 namespace Example.ChromiumFX.Vue.UI
 {
@@ -24,7 +25,16 @@ namespace Example.ChromiumFX.Vue.UI
         private void WcBrowser_OnDisplay(object sender, Neutronium.Core.Navigation.DisplayEvent e)
         {
             var windowCfx = wcBrowser.WPFWebWindow as IWPFCfxWebWindow;
-            windowCfx.ChromiumWebBrowser.DragHandler.OnDragEnter += DragHandler_OnDragEnter;
+            var cfxBrowser = windowCfx.ChromiumWebBrowser;
+            cfxBrowser.DragHandler.OnDragEnter += DragHandler_OnDragEnter;
+            cfxBrowser.ContextMenuHandler.OnBeforeContextMenu += ContextMenuHandler_OnBeforeContextMenu;
+        }
+
+        private void ContextMenuHandler_OnBeforeContextMenu(object sender, Chromium.Event.CfxOnBeforeContextMenuEventArgs e)
+        {
+            var model = e.Model;
+            model.InsertItemAt(0,(int)ContextMenuId.MENU_ID_PRINT, "Print");
+            model.InsertSeparatorAt(1);
         }
 
         private void DragHandler_OnDragEnter(object sender, Chromium.Event.CfxOnDragEnterEventArgs e)
