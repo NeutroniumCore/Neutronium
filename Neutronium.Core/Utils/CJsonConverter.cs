11c86742b834cdcd3179b635500a1ae2ba805c62
desmaisons_david <desmaisons_david@hotmail.com>
2018-01-16 20:05:48 -0200

    Documentation for CJsonConverter

--- Neutronium.Core/Utils/CJsonConverter.cs
+++ Neutronium.Core/Utils/CJsonConverter.cs
@@ -6,10 +6,18 @@ using Neutronium.Core.Log;
 
 namespace Neutronium.Core.Utils
 {
+    /// <summary>
+    /// Helper class to export C# object into cjson format (circular json).
+    /// This format is compatible with neutronium-vue client: https://github.com/NeutroniumCore/neutronium-vue
+    /// using the neutronium-vm-loader: https://github.com/NeutroniumCore/neutronium-vm-loader
+    /// </summary>
     public class CJsonConverter
     {
         private readonly CSharpToJavascriptConverter _Converter;
 
+        /// <summary>
+        /// Instanciate a new CJsonConverter
+        /// </summary>
         public CJsonConverter()
         {
             var cache = new SessionCacher();
@@ -17,12 +25,23 @@ namespace Neutronium.Core.Utils
             _Converter = new CSharpToJavascriptConverter(cache, factory, new NullLogger());
         }
 
+        /// <summary>
+        /// raw conversion to cjson format
+        /// </summary>
+        /// <param name="object">object to be serialiazed</param>
+        /// <returns>cjson string value</returns>
         public string ToCjson(object @object)
         {
             var glue = _Converter.Map(@object);
             return glue.AsCircularJson();
         }
 
+        /// <summary>
+        /// Conversion to cjson format compatible with a data context root Vm
+        /// Can be used as entry for neutronium-vm-loader https://github.com/NeutroniumCore/neutronium-vm-loader
+        /// </summary>
+        /// <param name="object">object to be serialiazed</param>
+        /// <returns>cjson string value</returns>
         public string ToRootVmCjson(object @object)
         {
             var context = new DataContextViewModel(@object);
