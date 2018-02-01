8a8d914cc6db6afe9b7eb9aef8c9e91e6a010484
desmaisons_david <desmaisons_david@hotmail.com>
2018-01-31 22:00:31 -0200

    REact build improvement

--- Examples/Example.ChromiumFx.Mobx.UI/View/mainview/config/webpack.config.prod.js
+++ Examples/Example.ChromiumFx.Mobx.UI/View/mainview/config/webpack.config.prod.js
@@ -35,7 +35,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+const cssFilename = 'static/css/[name].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -144,7 +144,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
-              name: '/static/media/[name].[hash:8].[ext]',
+              name: '/static/media/[name].[ext]',
             },
           },
           // Process JS with Babel.
@@ -228,7 +228,7 @@ module.exports = {
             // by webpacks internal loaders.
             exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
-              name: '/static/media/[name].[hash:8].[ext]',
+              name: '/static/media/[name].[ext]',
             },
           },
           // ** STOP ** Are you adding a new loader?
