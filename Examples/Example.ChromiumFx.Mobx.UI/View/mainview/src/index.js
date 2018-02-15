3682d574b9631ce58a17c72ca581a6730d65990d
WAY2\david.desmaisons <david.desmaisons@way2.com.br>
2018-02-15 20:30:42 -0200

    Removing service worker-react

--- Examples/Example.ChromiumFx.Mobx.UI/View/mainview/src/index.js
+++ Examples/Example.ChromiumFx.Mobx.UI/View/mainview/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { ready } from './mobxManager';
 import 'bootstrap/dist/css/bootstrap.css';
 
-
-ready.then(res =>{
+ready.then(res => {
     ReactDOM.render(<App viewModel={res.ViewModel}/>, document.getElementById('root'));
-    registerServiceWorker();
 })
