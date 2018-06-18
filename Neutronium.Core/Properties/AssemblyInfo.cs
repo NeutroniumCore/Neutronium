7cdec8734263f0bbceea99bd323fd87cd0970bdd
WAY2\david.desmaisons <david.desmaisons@way2.com.br>
2018-06-18 17:07:42 -0300

    Improving cproj meta-data

--- Neutronium.Core/Properties/AssemblyInfo.cs
+++ Neutronium.Core/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: InternalsVisibleTo("IntegratedTest")]
 [assembly: InternalsVisibleTo("Neutronium.Core.Test")]
 [assembly: InternalsVisibleTo("VueFramework.Test")]
-[assembly: InternalsVisibleTo("HTMLBindingTests")]
-[assembly: InternalsVisibleTo("WebBrowserEngineTests")]
+[assembly: InternalsVisibleTo("Tests.Universal.HTMLBindingTests")]
+[assembly: InternalsVisibleTo("Tests.Universal.WebBrowserEngineTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
