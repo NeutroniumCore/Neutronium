243be39348f30a32711dd61d3644352f34d2439c
desmaisons_david <desmaisons_david@hotmail.com>
2018-11-15 00:26:37 -0200

    Introducing IUiDispatcher

---
+++ Neutronium.Core/WebBrowserEngine/Window/IUiDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Neutronium.Core.WebBrowserEngine.Window
+{
+    /// <summary>
+    /// Abstraction of a dispatcher designed for the Ui thread
+    /// </summary>
+    public interface IUiDispatcher: IDispatcher
+    {
+        /// <summary>
+        /// Run on a safe thread the corresponding action
+        /// in a none-blocking manner.
+        /// The action should be given a binding priority
+        /// which is tipically slighty lower than default
+        /// priority.
+        /// No task is return for performance optimization.
+        /// This action should not be executed immediatly
+        /// even if the execution is currently in context.
+        /// </summary>
+        /// <param name="act">
+        /// Action to be executed
+        /// </param>
+        void DispatchWithBindingPriority(Action act);
+    }
+}
