8085fb8cf6f023c246a719366844cc54e319c82a
David Desmaisons <desmaisons_david@hotmail.com>
2018-11-23 12:50:56 -0200

    Migrating to .Net 472
    # Conflicts:
    #	.vs/Neutronium/DesignTimeBuild/.dtbcache

--- Examples/Example.ChromiumFx.Vue.Chromeless/Properties/Settings.Designer.cs
+++ Examples/Example.ChromiumFx.Vue.Chromeless/Properties/Settings.Designer.cs
@@ -8,17 +8,15 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Example.ChromiumFx.Vue.Chromeless.Properties
-{
-
-
+namespace Example.ChromiumFx.Vue.Chromeless.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
+        
         public static Settings Default {
             get {
                 return defaultInstance;
