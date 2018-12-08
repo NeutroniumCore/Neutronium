86eb73ebfc4cd5c4eb7d3dfa40d36f3018d36c75
desmaisons_david <desmaisons_david@hotmail.com>
2018-12-08 20:59:05 -0200

    Migrating example to vue-cli-neutronium

--- Examples/Example.Localization.Cfx.Vue/View/Main/src/App.vue
+++ Examples/Example.Localization.Cfx.Vue/View/Main/src/App.vue
@@ -8,7 +8,7 @@
         <div class="col-md-2">
             <span>{{Localization.Language}}</span>
             <select v-model="Langage">
-                <option v-for="lang in Langages" :value="lang">{{lang}}</option>
+                <option v-for="lang in Langages" :value="lang" :key="lang">{{lang}}</option>
             </select>
         </div>
 
@@ -21,40 +21,34 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/less/font-awesome.less';
-import rawDisplay from './components/RawDisplay'
-import commandButton from './components/command-button'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/less/font-awesome.less";
 
-const props={
-    viewModel: Object
+const props = {
+  viewModel: Object
 };
 
 export default {
-    name: 'app',
-    props,
-    components:{
-        rawDisplay,
-        commandButton
-    },
-    data () {
-        return this.viewModel
-    }
-}
+  name: "app",
+  props,
+  data() {
+    return this.viewModel;
+  }
+};
 </script>
 
 <style>
-    #app .logo {
-        text-align: center;
-    }
+#app .logo {
+  text-align: center;
+}
 
-    #app .logo img {
-        width: 100px;
-    }
+#app .logo img {
+  width: 100px;
+}
 
-    .panel-body {
-        position: relative;
-        min-height: 200px;
-        height: 100%;
-    }
+.panel-body {
+  position: relative;
+  min-height: 200px;
+  height: 100%;
+}
 </style>
