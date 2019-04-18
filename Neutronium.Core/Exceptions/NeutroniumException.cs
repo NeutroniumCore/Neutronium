ce2978f374e80a0c9ac534a210f0596e1bf5b2b6
David <desmaisons_david@hotmail.com>
2019-04-18 18:42:03 -0300

    wip

--- Neutronium.Core/Exceptions/NeutroniumException.cs
+++ Neutronium.Core/Exceptions/NeutroniumException.cs
@@ -4,15 +4,25 @@ namespace Neutronium.Core.Exceptions
 {
     public class NeutroniumArgumentException : ArgumentException
     {
-        public NeutroniumArgumentException(string message) : base(message)
+        public string Url { get; }
+        public NeutroniumArgumentException(string message, string url) : base(NeutroniumException.BuildMessage(message, url))
         {
+            Url = url;
         }
     }
 
     public class NeutroniumException : Exception
     {
-        public NeutroniumException(string message) : base(message)
+        public string Url { get; }
+        public NeutroniumException(string message, string url=null) : base(BuildMessage(message, url))
         {
+            Url = url;
+        }
+
+        internal static string BuildMessage(string message, string url = null)
+        {
+            return (url == null) ? message:  $"{message}. Please check: " + $"https://neutroniumcore.github.io/Neutronium/{url}";
         }
     }
-}
+ }
+
--- Neutronium.Core/Exceptions/NeutroniumExceptionHelper.cs
+++ Neutronium.Core/Exceptions/NeutroniumExceptionHelper.cs
@@ -4,19 +4,19 @@ namespace Neutronium.Core.Exceptions
 {
     public static class NeutroniumExceptionHelper
     {
-        public static Exception Get(string message)
+        public static Exception Get(string message, string url = null)
         {
-            return new NeutroniumException(message);
+            return new NeutroniumException(message, url);
         }
 
         public static Exception GetUnexpected()
         {
-            return Get("Unexpected error");
+            return Get("Unexpected error", null);
         }
 
-        public static ArgumentException GetArgument(string message)
+        public static ArgumentException GetArgument(string message, string url = null)
         {
-            return new NeutroniumArgumentException(message);
+            return new NeutroniumArgumentException(message, url);
         }
     }
 }
