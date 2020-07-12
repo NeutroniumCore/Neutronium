838bde96362a39ade0225786ed3d0a1badfa83b1
David <desmaisons_david@hotmail.com>
2020-07-12 19:57:42 -0300

    Creating GC agnostic baisc type

--- Neutronium.Core/Binding/GlueObject/Basic/JsBasicTyped.cs
+++ Neutronium.Core/Binding/GlueObject/Basic/JsBasicTyped.cs
@@ -1,50 +1,18 @@
-﻿using System;
-using Neutronium.Core.Binding.Listeners;
-using Neutronium.Core.WebBrowserEngine.JavascriptObject;
+﻿using Neutronium.Core.WebBrowserEngine.JavascriptObject;
 
 namespace Neutronium.Core.Binding.GlueObject.Basic
 {
-    internal abstract class JsBasicTyped<T>
+    internal abstract class JsBasicTyped<T> : JsBasicTypedGCAgnostic<T>
     {
-        public IJavascriptObject JsValue { get; private set; }
-        public T TypedValue { get; }
-        public object CValue => TypedValue;
-        public JsCsGlueType Type => JsCsGlueType.Basic;
-        public uint JsId => 0;
-
         public bool Release() => false;
 
         public IJsCsGlue AddRef() => (IJsCsGlue)this;
 
-        internal JsBasicTyped(T value)
+        internal JsBasicTyped(T value) : base(value)
         {
-            TypedValue = value;
         }
 
-        internal JsBasicTyped(IJavascriptObject jsValue, T value)
-        {
-            TypedValue = value;
-            JsValue = jsValue;
-        }
-
-        protected void SetJsValue(IJavascriptObject value)
-        {
-            JsValue = value;
-        }
-
-        public void VisitDescendants(Func<IJsCsGlue, bool> visit)
-        {
-            visit((IJsCsGlue)this);
-        }
-
-        public void VisitChildren(Action<IJsCsGlue> visit) { }
-
-        public void BuilString(IDescriptionBuilder context)
-        {
-            context.Append(ToString());
-        }
-
-        public void ApplyOnListenable(IObjectChangesListener listener)
+        internal JsBasicTyped(IJavascriptObject jsValue, T value) : base(jsValue, value)
         {
         }
     }
---
+++ Neutronium.Core/Binding/GlueObject/Basic/JsBasicTypedGCAgnostic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Neutronium.Core.Binding.Listeners;
+using Neutronium.Core.WebBrowserEngine.JavascriptObject;
+
+namespace Neutronium.Core.Binding.GlueObject.Basic
+{
+    internal abstract class JsBasicTypedGCAgnostic<T>
+    {
+        public IJavascriptObject JsValue { get; private set; }
+        public T TypedValue { get; }
+        public object CValue => TypedValue;
+        public JsCsGlueType Type => JsCsGlueType.Basic;
+        public uint JsId => 0;
+
+        protected JsBasicTypedGCAgnostic(T value)
+        {
+            TypedValue = value;
+        }
+
+        protected JsBasicTypedGCAgnostic(IJavascriptObject jsValue, T value)
+        {
+            TypedValue = value;
+            JsValue = jsValue;
+        }
+
+        protected void SetJsValue(IJavascriptObject value)
+        {
+            JsValue = value;
+        }
+
+        public void VisitDescendants(Func<IJsCsGlue, bool> visit)
+        {
+            visit((IJsCsGlue)this);
+        }
+
+        public void VisitChildren(Action<IJsCsGlue> visit) { }
+
+        public void BuilString(IDescriptionBuilder context)
+        {
+            context.Append(ToString());
+        }
+
+        public void ApplyOnListenable(IObjectChangesListener listener)
+        {
+        }
+    }
+}
