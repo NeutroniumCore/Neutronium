e86a9af8244bc0ea0f016f4f422c3681721e30a1
David <desmaisons_david@hotmail.com>
2020-07-13 00:48:58 -0300

    Creating garbage basic and using for string

---
+++ Neutronium.Core/Binding/GlueObject/Basic/JsBasicGarbageCollectedTyped.cs
@@ -0,0 +1,25 @@
+﻿using Neutronium.Core.WebBrowserEngine.JavascriptObject;
+
+namespace Neutronium.Core.Binding.GlueObject.Basic
+{
+    internal abstract class JsBasicGarbageCollectedTyped<T> : JsBasicTypedGcAgnostic<T>
+    {
+        private int _Count = 0;
+
+        public bool Release() => (--_Count == 0);
+
+        public IJsCsGlue AddRef()
+        {
+            _Count++;
+            return (IJsCsGlue) this;
+        }
+
+        internal JsBasicGarbageCollectedTyped(T value) : base(value)
+        {
+        }
+
+        internal JsBasicGarbageCollectedTyped(IJavascriptObject jsValue, T value) : base(jsValue, value)
+        {
+        }
+    }
+}
